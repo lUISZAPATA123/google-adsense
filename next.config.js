5336d428996b5f77c49bc9ef8e16d5a30ffecc55
Steven <steven@email.com>
2021-11-25 03:11:48 -0500

    crack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  reactStrictMode: true,
   images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
     domains: ["res.cloudinary.com"],
+    path: "/_next/image",
+    loader: "default",
   },
 };
